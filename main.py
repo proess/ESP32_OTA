333355d550ef3d2d80cfc465cbb7f2652b8a08d8
proess <mail@roepsa.de>
2025-03-19 18:42:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a test file of the OTA code
 
-print('Hello World-version 2')
+print('Hello World-version 3')
 
 print("this is another line test, written on an iPad using VS Code for web.")
